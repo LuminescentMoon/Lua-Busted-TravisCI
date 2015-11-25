d31e57304bb252aa041ea50899cc59f0ae119c84
Howard Nguyen <Luminess@users.noreply.github.com>
2015-11-24 23:42:25 -0800

    Create usage_spec.lua

---
+++ spec/usage_spec.lua
@@ -0,0 +1,7 @@
+local Testee = require('main')
+
+describe('tests of the testing the testifications of the testificates tests', function()
+  it('literally does nothing. literally.', function()
+    -- Literally nothing.
+  end)
+end)

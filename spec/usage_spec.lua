5a61e9e5e48264e4dde4c264c60ad742b049cdd0
Howard Nguyen <Luminess@users.noreply.github.com>
2015-11-25 09:30:46 -0800

    Update usage_spec.lua

--- spec/usage_spec.lua
+++ spec/usage_spec.lua
@@ -1,7 +1,9 @@
 local Testee = require('main')
 
-describe('tests of the testing the testifications of the testificates tests', function()
+describe('tests from testing the testifications of the testificates tests', function()
   it('literally does nothing. literally.', function()
     -- Literally nothing.
+    print(Testee)
+    assert.are.equal(1 + 1, 2)
   end)
 end)
